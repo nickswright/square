512dca1fe0b2cc0e1f97d00f9a5e32826ad7877c
Nicholas Wright <nickswright98@gmail.com>
2023-10-31 10:47:11 -0700

    More files

--- R/square.R
+++ R/square.R
@@ -1,3 +1,12 @@
+#' Squares numbers
+#'
+#' @param x a numeric atomic, vector, or list
+#'
+#' @return the square of \code{x}
+#' @export
+#'
+#' @examples
+#' square(10)
 square <- function(x) {
   x^2
 }
